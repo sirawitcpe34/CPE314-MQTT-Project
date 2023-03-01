18fd9e84396ce7ca7b108c1c4562c917cbfdc80b
Suppakorn Rakna <tap.pt.33@gmail.com>
2023-03-01 16:47:06 +0700

    `init`: publish

---
+++ mqtt_broker.py
@@ -0,0 +1,7 @@
+"""
+MQTT Broker forwards any data it receives to its subscribers.
+
+Constraints:
+- Broker prints out an IP address on the screen when a new subscriber or publisher connects or disconnects.
+- Broker prints out published messages on the screen.
+"""
