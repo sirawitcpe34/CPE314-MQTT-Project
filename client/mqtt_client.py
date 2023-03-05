f4a2cce24c83d60e9d3d2c2bf11db5a6f92619f8
jate-koh <jate_ben@hotmail.com>
2023-03-05 17:08:33 +0700

    `feat`: add data publish in chunks

--- client/mqtt_client.py
+++ client/mqtt_client.py
@@ -12,6 +12,7 @@
 
 # import threading
 import time
+import math
 
 import paho.mqtt.client as mqtt
 from .sensor_reader import SensorReader
@@ -51,11 +52,40 @@ class MQTTClient():
 
         # Send sensor data
         for i in range(stream.shape[0]):
+            print("Sending data row "+str(i))
             data = stream.get_data_by_row(i)
-            payload = f"{node_id};{data['time']};{data['humidity']};{data['temperature']};{data['thermal_array']}"
+            data_string = f"{node_id};{data['time']};{data['humidity']};{data['temperature']};{data['thermal_array']}"
+            data_size = len(data_string)
+            info = f"{node_id};{data_size}"
+
+            # Send info
+            self.client.publish("client/log", f"client sending information {data_size} to {self.node_id}/info")
+            self.client.publish(f"{self.node_id}/info", info)
+
+            # Check payload size
+            if data_size > MAX_PAYLOAD_SIZE:
+                chunks_size = MAX_PAYLOAD_SIZE - len(node_id) - len(str(data_size)) - 2
+                chunks = [data_string[i:i+chunks_size] for i in range(0, data_size, chunks_size)]
+
+
+
+                # Send each chunks as a separate payload along with node_id and data_size as prefixes
+                self.client.publish("client/log", f"client {self.node_id} requested {data_size} byte(s)")
+                for chunk in chunks:
+                    payload = f"{node_id}/{data_size}/{chunk}" # node id/expected length/data chunk
+                    self.client.publish("client/log", f"client {self.node_id} sent {len(payload)} byte(s) in {self.topic}")
+                    # self.client.publish("client/log", f"{payload}")
+                    self.client.publish(self.topic, payload)              
+                time.sleep(READ_INTERVAL)
+            
+            else:
+                # Send data in one message
+                payload = f"{node_id}/{data_size}/{data_string}"
+                self.client.publish(self.topic, payload)
+                time.sleep(READ_INTERVAL)
+
+                
             self.client.publish("client/log", f"client {self.node_id} sent {self.topic}")
-            self.client.publish(self.topic, payload)
-            time.sleep(READ_INTERVAL)
 
         self.client.publish(f'client/log', f'client {self.node_id} disconnected')
         self.client.disconnect()
@@ -105,4 +135,5 @@ class MQTTClient():
 
     for client in clients:
         client.join()
+
 """
--- server/mqtt_server.py
+++ server/mqtt_server.py
@@ -24,8 +24,10 @@ class MQTTServer():
         self.broker_ip = broker_ip
         self.broker_port = broker_port
         self.db_path = db_path
+        self.buffersize = 0;
+        self.buffer = ''
+        self.data_chunks = {}
         self.topics = topics
-
         self.client = mqtt.Client()
         self.client.connect(self.broker_ip, self.broker_port, 10)
         self.client.on_connect = self.on_connect
@@ -51,9 +53,36 @@ class MQTTServer():
         self.client.publish(f"server/log", f"server {ip}:{port} disconnected")
 
     def on_message(self, client, userdata, msg):
-        node_id, time, humidity, temperature, thermal_array = msg.payload.decode().split(';')
-        self.db.insert_data(node_id, time, humidity,
-                            temperature, thermal_array)
+
+        if msg.topic.endswith('data'):
+            print(f"Received messages: {msg.payload.decode()}")
+            
+            # Split the message into its components
+            node_id, expected_length, data_chunk = msg.payload.decode().split('/')
+            
+            # Convert the expected length to an integer
+            expected_length = int(expected_length)
+            if node_id not in self.data_chunks:
+                # Start a new list for this node_id
+                self.data_chunks[node_id] = [data_chunk]
+            else:
+                # Append the data chunk to the existing list
+                self.data_chunks[node_id].append(data_chunk)
+
+            # Check if all the chunks have been received
+            if sum(len(chunk) for chunk in self.data_chunks[node_id]) == expected_length:
+                
+                # Concatenate the chunks into a single string
+                data_string = ''.join(self.data_chunks[node_id])
+                
+                # Split the data string into its components
+                node_id, time, humidity, temperature, thermal_array = data_string.split(';')
+                
+                # Insert the data into the database
+                self.db.insert_data(node_id, time, humidity, temperature, thermal_array)
+                
+                # Reset the list of chunks for this node_id
+                self.data_chunks[node_id] = []
 
 """
 if __name__ == '__main__':
